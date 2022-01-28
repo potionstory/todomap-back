49c8fdadeeaa95dfc0f7fccf1250f1820e1e0139
potionstory <potionstory@gmail.com>
2022-01-28 09:48:33 +0900

    [FEAT] getUsers API Deprecated

--- functions/src/route/user/index.ts
+++ functions/src/route/user/index.ts
@@ -1,9 +1,8 @@
 import Router from "koa-router";
-import { getUser, getUsers, signIn, signUp } from "./user";
+import { getUser, signIn, signUp } from "./user";
 
 const route = new Router();
 
-route.get("/", getUsers);
 route.post("/signIn", signIn);
 route.post("/signUp", signUp);
 route.get("/:id", getUser);
